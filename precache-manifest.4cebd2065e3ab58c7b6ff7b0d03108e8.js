ee88051962ea5188828062a76b8732eac0c68c23
mahdis <mahdis.mhoseini1995@gmail.com>
2021-03-25 15:51:06 +0430

    Updates

---
+++ precache-manifest.4cebd2065e3ab58c7b6ff7b0d03108e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa0471956ea1bb60da2",
+    "url": "/Facial-Recognition-Web-App/static/css/main.68530754.chunk.css"
+  },
+  {
+    "revision": "7aa0471956ea1bb60da2",
+    "url": "/Facial-Recognition-Web-App/static/js/main.7aa04719.chunk.js"
+  },
+  {
+    "revision": "a1cd25147b316c1c4647",
+    "url": "/Facial-Recognition-Web-App/static/js/1.a1cd2514.chunk.js"
+  },
+  {
+    "revision": "84f9ebda83735a78eaed",
+    "url": "/Facial-Recognition-Web-App/static/js/runtime~main.84f9ebda.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/Facial-Recognition-Web-App/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "cc46e63d574915a8430eed2a3f4fe165",
+    "url": "/Facial-Recognition-Web-App/static/media/example.cc46e63d.jpg"
+  },
+  {
+    "revision": "775394708c85302233d6caa49b236502",
+    "url": "/Facial-Recognition-Web-App/index.html"
+  }
+];
